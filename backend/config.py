23811fb64bd1547d24f5cdea25f8612a3ffacb8e
edgarrossato <edgar.rossato@gmail.com>
2025-11-16 01:56:50 -0300

    delete

--- backend/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config:
-    #SQLALCHEMY_DATABASE_URI = 'sqlite:///cupcakeapp.db'
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://admin:ABH3mSHL6RMF6vcup5GnEBkN0PmzeDt6@dpg-d4773163jp1c73bqnubg-a.oregon-postgres.render.com/cupcakeapp")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv("SECRET_KEY", "chave_secreta")
-   
